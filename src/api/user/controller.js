c1b4fa007df7ad48a2e37c1addaf4538ac043c80
Wojciech Mazur <wojciech.mazur95@gmail.com>
2018-03-17 11:31:56 +0100

    Implemented base User model and routing

---
+++ src/api/user/controller.js
@@ -0,0 +1,86 @@
+import { success, notFound, errorHandler} from '../../services/response/'
+import {mongo, Types, ObjectId} from 'mongoose'
+import {Trainer} from './model'
+
+mongo.Promise = Promise;
+
+
+export const index = (req, res, next) => {
+    Trainer.find(
+        ("query" in req.query) ? JSON.parse(req.query['query']) : {},
+    ).limit(parseInt(req.query['limit'])  || 25)
+     .skip(parseInt(req.query['offset']) || 0)
+     .sort( ("sort" in req.query) ? JSON.parse(req.query['sort']) : {surname: 1},)
+    .then(
+        docs => {
+         success(res,200)(docs);
+        },
+        err => {
+            errorHandler(res)(err)
+        }
+     );
+};
+
+export const show = (req, res, next) => {
+    Trainer.findOne({
+            "_id": req.params.id
+        }
+    ).then(
+        doc => {
+            if (doc !== null)
+                success(res)(doc);
+            else
+                notFound(res)(req.params)
+        },
+        err => errorHandler(res)(err)
+    )
+};
+
+export const create = (req, res, next) => {
+    Trainer.findOne(
+        {
+            $and:[
+                {name: req.body.name},
+                {surname: req.body.surname},
+                {email: req.body.email}
+            ]
+        }, (err, trainer) => {
+        if(err)
+            res.sendStatus(400)(err);
+        if(trainer===null){
+            console.log("creating");
+            let trainer = new Trainer(req.body);
+            Trainer.create(trainer);
+            res.status(201).end(JSON.stringify(trainer));
+        }else{
+            console.log("Already exists");
+            res.sendStatus(409);
+        }
+    });
+};
+
+
+export const edit = (req, res, next) => {
+    const id = req.params.id;
+    const body =req.body;
+
+    Trainer.findById(id)
+        .then((doc) => doc ? Object.assign(doc, body).save() : null)
+        .then((response) => success(res)(response))
+        .catch((err) => errorHandler(res)(err))
+};
+
+export const remove = (req, res, next) => {
+    const conditions = ("query" in req.query) ? JSON.parse(req.query['query']) : {
+        "_id": req.params.id
+    };
+    Trainer.find(conditions)
+        .then((docs) => {
+            Trainer.remove(conditions)
+                .then(() => success(res)(docs))
+                .catch((err) => errorHandler(res)(err))
+        })
+        .catch((err) => errorHandler(res)(err));
+};
+
+
---
+++ src/api/user/model.js
@@ -0,0 +1,22 @@
+import config from "../../config";
+const mongoose = require("mongoose");
+
+const trainerSchema = new mongoose.Schema({
+    name: {type: String, required: true, trim: true},
+    surname: {type: String, required: true, trim: true},
+    specialization: [String],
+    info: String,
+    email: {type: String, unique: true, required: true, match:/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/},
+    phone: {type: String, unique: true, required: true, match: /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/}
+}, {
+    versionKey: '_version',
+    timestamps: true
+});
+
+mongoose.connect(`${config.mongoURL}/${config.dbName}`).catch(
+    err => console.log(`Error while connecting to DB: ${err}`)
+);
+
+export const Trainer = mongoose.model('Trainer', trainerSchema, 'Trainers');
+
+
