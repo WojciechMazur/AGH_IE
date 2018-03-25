9e72bf32ad39b76dd15d43fb9b9ac8caae2c72e7
Wojciech Mazur <wojciech.mazur95@gmail.com>
2018-03-25 10:25:29 +0200

    Allowed CORS, added basic loggin middleware.

--- backend/src/services/express/index.js
+++ backend/src/services/express/index.js
@@ -21,6 +21,15 @@ export default (apiRoot, routes) => {
             next();
         }
     });
+    app.use((req,res, next)=>{
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+    app.use((req, res, next)=>{
+       console.log(`${new Date().toISOString()}, ${req.originalUrl}, ${JSON.stringify(req.body)}`);
+        next()
+    });
     app.use(apiRoot, routes);
 
     return app
